402fe47284701155da099154273622120e5f008a
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-04-24 09:11:23 +0200

    Upgrade CLI11 and fmt

--- conanfile.py
+++ conanfile.py
@@ -9,8 +9,8 @@ class PFFDTD(ConanFile):
     generators = 'CMakeToolchain', 'CMakeDeps'
 
     def requirements(self):
-        self.requires('cli11/2.4.2')
-        self.requires('fmt/11.1.3')
+        self.requires('cli11/2.5.0')
+        self.requires('fmt/11.1.4')
 
         if self.settings.os != 'Macos':
             self.requires('hdf5/1.14.5')
