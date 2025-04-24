57bac70688f7b3d0793e1d68bb6ddc2fc4aaaff7
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-04-24 09:02:31 +0200

    [python] More filters

---
+++ src/python/pffdtd/filters/fir.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025 Tobias Hienzsch
+
+
+import numpy as np
+
+
+def main():
+    fs = 96000
+    taps = 3072*2+1
+    taps = 1024
+    taps = fs/1000*20
+    resolution = fs/taps
+    group_delay = (taps-1)/2
+    print(f'{fs=}Hz')
+    print(f'{taps=}')
+    print(f'{resolution=:.2f}Hz')
+    print(f'{group_delay=} samples')
+    print(f'group_delay={1000/fs*group_delay:.2f} ms')
+
+
+if __name__ == '__main__':
+    main()
