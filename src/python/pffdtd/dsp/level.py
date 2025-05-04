fb0210426383bf5e92e2947ba483513eef4d29cd
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-05-04 06:29:19 +0200

    [python] Add x_ref to normalize_to_RMS_dBFS

--- src/python/pffdtd/dsp/level.py
+++ src/python/pffdtd/dsp/level.py
@@ -10,8 +10,17 @@ def crest_factor(x):
     return peak/rms
 
 
-def normalize_to_RMS_dBFS(x, target_dBFS):
-    rms = np.sqrt(np.mean(x**2))
+def normalize_to_RMS_dBFS(
+    x: np.ndarray,
+    target_dBFS: float,
+    *,
+    x_ref: np.ndarray | None = None,
+) -> np.ndarray:
+    if x_ref is None:
+        rms = np.sqrt(np.mean(x**2))
+    else:
+        rms = np.sqrt(np.mean(x_ref**2))
+
     current_dB = 20 * np.log10(rms)
     gain_dB = target_dBFS - current_dB
     gain = 10**(gain_dB / 20)
