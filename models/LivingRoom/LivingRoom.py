2c1385222a1150e4e54c8d520bcd11092508d0d8
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-05-25 06:00:10 +0200

    [models] Improve LivingRoom

--- models/LivingRoom/LivingRoom.py
+++ models/LivingRoom/LivingRoom.py
@@ -14,16 +14,16 @@ from pffdtd.sim3d.setup import Setup3D
 class LivingRoom(Setup3D):
     model_file = 'model.json'
     mat_folder = '../../sim_data/LivingRoom/materials'
-    source_index = 1
-    source_signal = 'impulse-highpass'
+    source_index = 2
+    source_signal = 'impulse-highpass-40'
     diff_source = True
     materials = {
         'Book Shelf': 'wood.h5',
         'Ceiling': 'concrete_painted.h5',
         'Coffee Table': 'wood.h5',
-        'Couch': 'absorber_8000_100mm.h5',
+        'Couch': 'absorber_8000_150mm.h5',
         'Desk': 'wood.h5',
-        'Door': 'wood.h5',
+        # 'Door': 'wood.h5',
         'Floor': 'wood.h5',
         'Kallax': 'wood.h5',
         'Monitors': 'wood.h5',
@@ -53,14 +53,14 @@ class LivingRoom(Setup3D):
         iso_octaves = center_frequencies(1, 1000, 6, 5)
 
         # autopep8: off
-        absorber_8000_100mm = porous_absorber(0.1, 8000.0, frequency=iso_octaves, offset_zeros=True)
+        absorber_8000_150mm = porous_absorber(0.15, 8000.0, frequency=iso_octaves, offset_zeros=True, angle=45)
         concrete_painted    = np.array([0.01, 0.01, 0.01, 0.05, 0.06, 0.07, 0.09, 0.08, 0.08, 0.08, 0.08])
         glas_thick          = np.array([0.15, 0.30, 0.27, 0.18, 0.06, 0.04, 0.03, 0.02, 0.02, 0.02, 0.01])
         metal_iron          = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.02, 0.03, 0.03, 0.03, 0.02])
         wood                = np.array([0.10, 0.11, 0.13, 0.15, 0.11, 0.10, 0.07, 0.06, 0.07, 0.07, 0.07])
 
         folder = Path(self.mat_folder)
-        fit_to_Sabs_oct_11(absorber_8000_100mm , filename=folder / 'absorber_8000_100mm.h5')
+        fit_to_Sabs_oct_11(absorber_8000_150mm , filename=folder / 'absorber_8000_150mm.h5')
         fit_to_Sabs_oct_11(concrete_painted    , filename=folder / 'concrete_painted.h5'   )
         fit_to_Sabs_oct_11(glas_thick          , filename=folder / 'glas_thick.h5'         )
         fit_to_Sabs_oct_11(metal_iron          , filename=folder / 'metal_iron.h5'         )
@@ -73,11 +73,13 @@ class LivingRoom(Setup3D):
         dir = Path('.')
         obj = dir/'obj'
 
-        s1 = [3.65-0.5, 6.0-0.3, 1.2]
-        s2 = [3.65-0.5, 6.0-2.5, 1.2]
+        s1 = [3.65-0.54, 6.0-0.3, 1.2]
+        s2 = [3.65-0.54, 6.0-2.4, 1.2]
 
-        r1 = [0.6, 6.0-1.3, 1.1]
-        r2 = [0.6, 6.0-2.0, 1.1]
+        r1 = s2.copy()
+        r1[0] -= 0.7
+        r2 = [0.6, 6.0-1.3, 1.1]
+        r3 = [0.6, 6.0-2.0, 1.1]
 
         m = MeshModelBuilder()
         m.add('Book Shelf', obj / 'book_shelf.obj', [200, 200, 200], reverse=True)
@@ -85,7 +87,7 @@ class LivingRoom(Setup3D):
         m.add('Coffee Table', obj / 'coffee_table.obj', [10, 10, 10], reverse=True)
         m.add('Couch', obj / 'couch.obj', [29, 50, 112], reverse=True)
         m.add('Desk', obj / 'desk.obj', [103, 70, 55], reverse=True)
-        m.add('Door', obj / 'door.obj', [103, 70, 55], reverse=True)
+        # m.add('Door', obj / 'door.obj', [103, 70, 55], reverse=True)
         m.add('Floor', obj / 'floor.obj', [133, 94, 66], reverse=True)
         m.add('Kallax', obj / 'kallax.obj', [200, 200, 200], reverse=True)
         m.add('Monitors', obj / 'monitors.obj', [15, 15, 15], reverse=True)
@@ -100,7 +102,8 @@ class LivingRoom(Setup3D):
         m.add_source('S1', s1)
         m.add_source('S2', s2)
         m.add_receiver('R1', r1)
-        m.add_receiver('R2', r2)
+        # m.add_receiver('R2', r2)
+        # m.add_receiver('R3', r3)
         m.write(self.model_file)
 
     def _print(self, msg):
