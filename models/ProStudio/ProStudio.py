65fb50e4a1d5562922c2cb0e9bc011d116098eec
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-05-31 23:15:28 +0200

    [models] Update ProStudio

--- models/ProStudio/ProStudio.py
+++ models/ProStudio/ProStudio.py
@@ -4,8 +4,9 @@ from pathlib import Path
 
 import numpy as np
 
-from pffdtd.absorption.admittance import fit_to_Sabs_oct_11
+from pffdtd.absorption.admittance import convert_Sabs_to_Yn, fit_to_Sabs_oct_11, write_freq_ind_mat_from_Yn
 from pffdtd.absorption.porous import porous_absorber
+from pffdtd.analysis.rt60 import recommended_rt60
 from pffdtd.geometry.math import find_third_vertex, point_along_line, with_x_offset
 from pffdtd.signals.octave import center_frequencies
 from pffdtd.sim3d.model_builder import MeshModelBuilder
@@ -19,9 +20,10 @@ class ProStudio(Setup3D):
     source_signal = 'impulse'
     diff_source = True
     materials = {
-        'ATC Left': 'wood.h5',
-        'ATC Right': 'wood.h5',
-        'Ceiling': 'absorber_8000_200mm_gap_200mm.h5',
+        # 'ATC Left': 'wood.h5',
+        # 'ATC Right': 'wood.h5',
+        'Ceiling Back': 'absorber_8000_200mm_gap_200mm.h5',
+        'Ceiling Front': 'absorber_8000_200mm_gap_200mm.h5',
         'Console': 'metal_iron.h5',
         'Couch': 'leather_arm_chair.h5',
         # 'Diffusor': 'wood.h5',
@@ -30,11 +32,11 @@ class ProStudio(Setup3D):
         'Rack': 'wood.h5',
         'Raised Floor': 'wood.h5',
         'Walls Back': 'absorber_8000_200mm_gap_200mm.h5',
-        'Walls Front': 'absorber_8000_200mm_gap_100mm.h5',
+        'Walls Front': 'Sabs_9512.h5',
         'Walls Side': 'absorber_8000_50mm.h5',
         'Windows': 'glas_thick.h5',
     }
-    duration = 2.0
+    duration = 1.0
     Tc = 20
     rh = 50
     fcc = False
@@ -42,7 +44,7 @@ class ProStudio(Setup3D):
     fmax = 1000.0
     save_folder = '../../sim_data/ProStudio/cpu'
     save_folder_gpu = '../../sim_data/ProStudio/gpu'
-    draw_vox = True
+    draw_vox = False
     draw_backend = 'polyscope'
     compress = 0
     rot_az_el = (0, 0)
@@ -61,6 +63,7 @@ class ProStudio(Setup3D):
         metal_iron                    = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.02, 0.03, 0.03, 0.03, 0.02])
         wood                          = np.array([0.10, 0.11, 0.13, 0.15, 0.11, 0.10, 0.07, 0.06, 0.07, 0.07, 0.07])
         wood_on_concrete              = np.array([0.01, 0.01, 0.01, 0.04, 0.04, 0.07, 0.06, 0.06, 0.07, 0.06, 0.06])
+        Sabs_9512                     = convert_Sabs_to_Yn(0.9512)
 
         fit_to_Sabs_oct_11(absorber_8000_50mm            , filename=folder / 'absorber_8000_50mm.h5'           )
         fit_to_Sabs_oct_11(absorber_8000_200mm_gap_100mm , filename=folder / 'absorber_8000_200mm_gap_100mm.h5')
@@ -70,26 +73,33 @@ class ProStudio(Setup3D):
         fit_to_Sabs_oct_11(metal_iron                    , filename=folder / 'metal_iron.h5'                   )
         fit_to_Sabs_oct_11(wood                          , filename=folder / 'wood.h5'                         )
         fit_to_Sabs_oct_11(wood_on_concrete              , filename=folder / 'wood_on_concrete.h5'             )
+        write_freq_ind_mat_from_Yn(Sabs_9512, filename=folder / 'Sabs_9512.h5')
         # autopep8: on
 
     def generate_model(self, constants):
+        volume = 165.45  # m^3
         self._print('Generate model')
+        self._print(f'Volume: {volume} m^3')
+        self._print(f'Target RT60: {recommended_rt60(volume)}')
+
         dir = Path('.')
         obj = dir/'obj'
 
         mul = 4.0 if self.fcc else 2.0
         offset = constants.h * mul
 
-        s1 = [3.38789-3.288/2, 6.90-offset, 1.062+0.332]
-        sub1 = s1.copy()
-        sub1[2] = 0.0 + offset
+        s1 = [1.735, 6.836-offset, 1.062+0.332]
+        sub1a = s1.copy()
+        sub1a[2] = 0.0 + offset
+        sub1b = sub1a.copy()
+        sub1b[0] += 1.0
 
         s2 = s1.copy()
         s2[0] += 3.288
-        sub2 = s2.copy()
-        sub2[2] = 0.0 + offset
-
-        s3 = [2.25, 3.55, 0.4]
+        sub2a = s2.copy()
+        sub2a[2] = 0.0 + offset
+        sub2b = sub2a.copy()
+        sub2b[0] -= 1.0
 
         r1 = list(find_third_vertex(s1, s2)[1])
         r1[1] += (1.0-0.0)
@@ -114,9 +124,10 @@ class ProStudio(Setup3D):
         r6 = with_x_offset(r2, +0.73*1.5)
 
         m = MeshModelBuilder()
-        m.add('ATC Left', obj / 'atc_left.obj', [5, 5, 5], reverse=True)
-        m.add('ATC Right', obj / 'atc_right.obj', [5, 5, 5], reverse=True)
-        m.add('Ceiling', obj / 'ceiling.obj', [100, 100, 100])
+        # m.add('ATC Left', obj / 'atc_left.obj', [5, 5, 5], reverse=True)
+        # m.add('ATC Right', obj / 'atc_right.obj', [5, 5, 5], reverse=True)
+        m.add('Ceiling Back', obj / 'ceiling_back.obj', [100, 100, 100])
+        m.add('Ceiling Front', obj / 'ceiling_front.obj', [100, 100, 100])
         m.add('Console', obj / 'console.obj', [60, 60, 60], reverse=True)
         m.add('Couch', obj / 'couch.obj', [5, 5, 48], reverse=True)
         # m.add('Diffusor', obj / 'diffusor.obj', [53, 33, 0], reverse=True)
@@ -130,9 +141,10 @@ class ProStudio(Setup3D):
         m.add('Windows', obj / 'windows.obj', [137, 207, 240], reverse=True)
         m.add_source('S1', s1)
         m.add_source('S2', s2)
-        # m.add_source("S3", s3)
-        # m.add_source("SUB1", sub1)
-        # m.add_source("SUB2", sub2)
+        m.add_source('SUB1A', sub1a)
+        m.add_source('SUB1B', sub1b)
+        m.add_source('SUB2A', sub2a)
+        m.add_source('SUB2B', sub2b)
         m.add_receiver('R1', r1)
         m.add_receiver('R2', r2)
         m.add_receiver('R3', r3)
