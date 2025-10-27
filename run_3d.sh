8117581bd3d6767baedfade26203365726c55ad5
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-10-28 00:02:51 +0100

    Add remote engine support for run_3d.sh

--- run_3d.sh
+++ run_3d.sh
@@ -29,7 +29,7 @@ cd "$model_dir"
 pffdtd sim3d setup "$sim_setup"
 
 # Run sim
-OMP_PLACES=cores OMP_PROC_BIND=close OMP_NUM_THREADS=16 $pffdtd_engine sim3d -e cuda -p "64" -s "$sim_dir"
+# OMP_PLACES=cores OMP_PROC_BIND=close OMP_NUM_THREADS=16 $pffdtd_engine sim3d -e cpu -p "64" -s "$sim_dir"
 # nsys profile --force-overwrite true -t cuda,osrt,nvtx --sample=none -o prof $pffdtd_engine sim3d -e cuda -p "64" -s "$sim_dir"
 # /usr/local/cuda-12.6/bin/ncu \
 #     --kernel-name-base demangled \
@@ -39,6 +39,16 @@ OMP_PLACES=cores OMP_PROC_BIND=close OMP_NUM_THREADS=16 $pffdtd_engine sim3d -e 
 #     $pffdtd_engine sim3d -e cuda -p "64" -s "$sim_dir"
 
 # pffdtd sim3d engine --sim_dir="$sim_dir" --plot --draw_backend="mayavi" --json_model="${model_dir}/model.json"
+
+local_dir="$sim_dir"
+remote_cmd="/home/tobante/Developer/tobanteAudio/pffdtd/build/src/cpp/pffdtd-engine sim3d -e cuda -p 64 -s ."
+remote_dir="/tmp/$sim_name"
+remote_host="tobante@192.168.0.16"
+
+ssh "$remote_host" "mkdir -p -- '$remote_dir'"
+rsync -az --delete --chmod=Du=rwx,Fu=rw,go-rwx -e ssh "$local_dir"/ "$remote_host":"$remote_dir"/
+ssh -t "$remote_host" "set -euo pipefail; cd -- '$remote_dir'; $remote_cmd"
+rsync -az -e ssh "$remote_host":"$remote_dir"/ "$local_dir"/
 
 # Post-process
 pffdtd sim3d process-outputs --sim_dir="$sim_dir" --resample_Fs 48000 --fcut_lowpass "$fmax" --order_lowpass=8 --symmetric_lowpass --fcut_lowcut "$fmin" --order_lowcut=4 --air_abs_filter="none" --save_wav --plot
@@ -54,7 +64,7 @@ pffdtd analysis summary --fmax="$fmax" --smoothing="$smoothing" $sim_dir/R001_ou
 # pffdtd analysis spectrogram --min_db=-60 $sim_dir/R030_out_normalised.wav
 # pffdtd analysis rt60 --fmin=$fmin --fmax="$fmax" --target=0.296 $sim_dir/R001_out_normalised.wav
 # pffdtd analysis rt60 --sim_dir="$sim_dir" --fmin=$fmin --fmax="$fmax" --target=0.25
-# pffdtd analysis room-modes --sim_dir="$sim_dir" --fmin=$fmin --num_modes=20 --width=4.79 --length=7.13 --height=3.44
+# pffdtd analysis room-modes --sim_dir="$sim_dir" --fmin=$fmin --num_modes=20 --width=3.65 --length=6.0 --height=3.12
 # pffdtd signals convolution --sim_dir="$sim_dir"
 # pffdtd analysis localization --sim_dir="$sim_dir" "$model_dir/model.json"
 # pffdtd diffusion measurement "$sim_dir"
